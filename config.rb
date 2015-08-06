af97c3f9cf4204abdd5d3107d206b5d19691c0f2
Patrik Martinsson <patrik.martinsson@smhi.se>
2015-08-06 19:10:53 +0200

    Initial setup

--- config.rb
+++ config.rb
@@ -4,11 +4,11 @@ require 'sass-globbing'
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
+http_path = "/ensamiasien.se/"
+http_images_path = "/ensamiasien.se/images"
 http_generated_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_fonts_path = "/ensamiasien.se/fonts"
+css_dir = "public/ensamiasien.se/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
